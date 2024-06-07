577acb1cd4a4503d798538c43271db839cff4ff1
Jason Stevens <jason.r.stevens@live.com>
2024-06-07 08:57:13 +0100

    build libgcc with native CC & define TARGET_MEM_FUNCTIONS

--- build-msvc.cmd
+++ build-msvc.cmd
@@ -1,12 +1,12 @@
 del hello.o hello.exe
 del insn-flags.h insn-config.h insn-codes.h insn-output.c insn-recog.c 
 del insn-emit.c insn-extract.c insn-peep.c 
-del  c-parse.tab.c c-parse.output cexp.tab.c  cexp.output
+del c-parse.tab.c c-parse.output cexp.tab.c cexp.output cexp.c
 del genemit.exe genoutput.exe genrecog.exe genextract.exe genflags.exe 
 del gencodes.exe genconfig.exe genpeep.exe cc1.exe cpp.exe cccp.exe xgcc.exe
 del *.co *.greg *.lreg *.combine *.flow *.cse *.jump *.rtl *.tree *.loop
 del *.obj *.o
-del libgcc.lib
+del gnulib.lib
 
 cl /c /Fotoplev.obj toplev.c
 cl /c /Foversion.obj version.c
@@ -100,46 +100,46 @@ del gencodes.exe genconfig.exe genpeep.exe
 del *.co *.greg *.lreg *.combine *.flow *.cse *.jump *.rtl *.tree *.loop
 del *.obj *.o
 
-xgcc -O -I. -c -DL_eprintf -o _eprintf.o gnulib.c
-xgcc -O -I. -c -DL_umulsi3 -o _umulsi3.o gnulib.c
-xgcc -O -I. -c -DL_mulsi3 -o _mulsi3.o gnulib.c
-xgcc -O -I. -c -DL_udivsi3 -o _udivsi3.o gnulib.c
-xgcc -O -I. -c -DL_divsi3 -o _divsi3.o gnulib.c
-xgcc -O -I. -c -DL_umodsi3 -o _umodsi3.o gnulib.c
-xgcc -O -I. -c -DL_modsi3 -o _modsi3.o gnulib.c
-xgcc -O -I. -c -DL_lshrsi3 -o _lshrsi3.o gnulib.c
-xgcc -O -I. -c -DL_lshlsi3 -o _lshlsi3.o gnulib.c
-xgcc -O -I. -c -DL_ashrsi3 -o _ashrsi3.o gnulib.c
-xgcc -O -I. -c -DL_ashlsi3 -o _ashlsi3.o gnulib.c
-xgcc -O -I. -c -DL_divdf3 -o _divdf3.o gnulib.c
-xgcc -O -I. -c -DL_muldf3 -o _muldf3.o gnulib.c
-xgcc -O -I. -c -DL_negdf2 -o _negdf2.o gnulib.c
-xgcc -O -I. -c -DL_adddf3 -o _adddf3.o gnulib.c
-xgcc -O -I. -c -DL_subdf3 -o _subdf3.o gnulib.c
-xgcc -O -I. -c -DL_cmpdf2 -o _cmpdf2.o gnulib.c
-xgcc -O -I. -c -DL_fixunsdfsi -o _fixunsdfsi.o gnulib.c
-xgcc -O -I. -c -DL_fixunsdfdi -o _fixunsdfdi.o gnulib.c
-xgcc -O -I. -c -DL_fixdfsi -o _fixdfsi.o gnulib.c
-xgcc -O -I. -c -DL_fixdfdi -o _fixdfdi.o gnulib.c
-xgcc -O -I. -c -DL_floatsidf -o _floatsidf.o gnulib.c
-xgcc -O -I. -c -DL_floatdidf -o _floatdidf.o gnulib.c
-xgcc -O -I. -c -DL_truncdfsf2 -o _truncdfsf2.o gnulib.c
-xgcc -O -I. -c -DL_extendsfdf2 -o _extendsfdf2.o gnulib.c
-xgcc -O -I. -c -DL_addsf3 -o _addsf3.o gnulib.c
-xgcc -O -I. -c -DL_negsf2 -o _negsf2.o gnulib.c
-xgcc -O -I. -c -DL_subsf3 -o _subsf3.o gnulib.c
-xgcc -O -I. -c -DL_cmpsf2 -o _cmpsf2.o gnulib.c
-xgcc -O -I. -c -DL_mulsf3 -o _mulsf3.o gnulib.c
-xgcc -O -I. -c -DL_divsf3 -o _divsf3.o gnulib.c
+cl /c /I. /Ox -DL_eprintf /Fo_eprintf.obj gnulib.c
+cl /c /I. /Ox -DL_umulsi3 /Fo_umulsi3.obj gnulib.c
+cl /c /I. /Ox -DL_mulsi3 /Fo_mulsi3.obj gnulib.c
+cl /c /I. /Ox -DL_udivsi3 /Fo_udivsi3.obj gnulib.c
+cl /c /I. /Ox -DL_divsi3 /Fo_divsi3.obj gnulib.c
+cl /c /I. /Ox -DL_umodsi3 /Fo_umodsi3.obj gnulib.c
+cl /c /I. /Ox -DL_modsi3 /Fo_modsi3.obj gnulib.c
+cl /c /I. /Ox -DL_lshrsi3 /Fo_lshrsi3.obj gnulib.c
+cl /c /I. /Ox -DL_lshlsi3 /Fo_lshlsi3.obj gnulib.c
+cl /c /I. /Ox -DL_ashrsi3 /Fo_ashrsi3.obj gnulib.c
+cl /c /I. /Ox -DL_ashlsi3 /Fo_ashlsi3.obj gnulib.c
+cl /c /I. /Ox -DL_divdf3 /Fo_divdf3.obj gnulib.c
+cl /c /I. /Ox -DL_muldf3 /Fo_muldf3.obj gnulib.c
+cl /c /I. /Ox -DL_negdf2 /Fo_negdf2.obj gnulib.c
+cl /c /I. /Ox -DL_adddf3 /Fo_adddf3.obj gnulib.c
+cl /c /I. /Ox -DL_subdf3 /Fo_subdf3.obj gnulib.c
+cl /c /I. /Ox -DL_cmpdf2 /Fo_cmpdf2.obj gnulib.c
+cl /c /I. /Ox -DL_fixunsdfsi /Fo_fixunsdfsi.obj gnulib.c
+cl /c /I. /Ox -DL_fixunsdfdi /Fo_fixunsdfdi.obj gnulib.c
+cl /c /I. /Ox -DL_fixdfsi /Fo_fixdfsi.obj gnulib.c
+cl /c /I. /Ox -DL_fixdfdi /Fo_fixdfdi.obj gnulib.c
+cl /c /I. /Ox -DL_floatsidf /Fo_floatsidf.obj gnulib.c
+cl /c /I. /Ox -DL_floatdidf /Fo_floatdidf.obj gnulib.c
+cl /c /I. /Ox -DL_truncdfsf2 /Fo_truncdfsf2.obj gnulib.c
+cl /c /I. /Ox -DL_extendsfdf2 /Fo_extendsfdf2.obj gnulib.c
+cl /c /I. /Ox -DL_addsf3 /Fo_addsf3.obj gnulib.c
+cl /c /I. /Ox -DL_negsf2 /Fo_negsf2.obj gnulib.c
+cl /c /I. /Ox -DL_subsf3 /Fo_subsf3.obj gnulib.c
+cl /c /I. /Ox -DL_cmpsf2 /Fo_cmpsf2.obj gnulib.c
+cl /c /I. /Ox -DL_mulsf3 /Fo_mulsf3.obj gnulib.c
+cl /c /I. /Ox -DL_divsf3 /Fo_divsf3.obj gnulib.c
 
 del gnulib.lib
-lib /OUT:gnulib.lib _adddf3.o _addsf3.o _ashlsi3.o _ashrsi3.o _cmpdf2.o _cmpsf2.o _divdf3.o ^
-_divsf3.o _divsi3.o _eprintf.o _extendsfdf2.o _fixdfdi.o _fixdfsi.o _fixunsdfdi.o ^
-_fixunsdfsi.o _floatdidf.o _floatsidf.o _lshlsi3.o _lshrsi3.o _modsi3.o _muldf3.o ^
-_mulsf3.o _mulsi3.o _negdf2.o _negsf2.o _subdf3.o _subsf3.o _truncdfsf2.o ^
-_udivsi3.o _umodsi3.o _umulsi3.o
+lib /OUT:gnulib.lib _adddf3.obj _addsf3.obj _ashlsi3.obj _ashrsi3.obj _cmpdf2.obj _cmpsf2.obj _divdf3.obj ^
+_divsf3.obj _divsi3.obj _eprintf.obj _extendsfdf2.obj _fixdfdi.obj _fixdfsi.obj _fixunsdfdi.obj ^
+_fixunsdfsi.obj _floatdidf.obj _floatsidf.obj _lshlsi3.obj _lshrsi3.obj _modsi3.obj _muldf3.obj ^
+_mulsf3.obj _mulsi3.obj _negdf2.obj _negsf2.obj _subdf3.obj _subsf3.obj _truncdfsf2.obj ^
+_udivsi3.obj _umodsi3.obj _umulsi3.obj
 
-del *.o
+del *.obj
 
 xgcc -c hello.c
 link /NODEFAULTLIB:libc.lib /NODEFAULTLIB:OLDNAMES.LIB  -out:hello.exe hello.o -entry:mainCRTStartup gnulib.lib LIBC.LIB KERNEL32.LIB
--- tm.h
+++ tm.h
@@ -1,3 +1,7 @@
+/* Implicit library calls should use memcpy, not bcopy, etc.  */
+
+#define TARGET_MEM_FUNCTIONS 1
+
 #include "tm-i386dos.h"
 /*#include "tm-i386v.h"	*/
 
